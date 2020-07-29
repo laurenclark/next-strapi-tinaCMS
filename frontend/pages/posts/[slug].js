abee8f73b645cbf49f59b0794fc0bb7634004e26
Lauren Clark <lauren.c4231@gmail.com>
2020-07-29 14:47:14 +0100

    Format

--- frontend/pages/posts/[slug].js
+++ frontend/pages/posts/[slug].js
@@ -12,73 +12,77 @@ import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-  return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
-    </Layout>
-  )
+    const router = useRouter()
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage statusCode={404} />
+    }
+    return (
+        <Layout preview={preview}>
+            <Container>
+                <Header />
+                {router.isFallback ? (
+                    <PostTitle>Loading…</PostTitle>
+                ) : (
+                    <>
+                        <article className="mb-32">
+                            <Head>
+                                <title>
+                                    {post.title} | Next.js Blog Example with{' '}
+                                    {CMS_NAME}
+                                </title>
+                                <meta
+                                    property="og:image"
+                                    content={post.ogImage.url}
+                                />
+                            </Head>
+                            <PostHeader
+                                title={post.title}
+                                coverImage={post.coverImage}
+                                date={post.date}
+                                author={post.author}
+                            />
+                            <PostBody content={post.content} />
+                        </article>
+                    </>
+                )}
+            </Container>
+        </Layout>
+    )
 }
 
 export async function getStaticProps({ params }) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'content',
-    'ogImage',
-    'coverImage',
-  ])
-  const content = await markdownToHtml(post.content || '')
+    const post = getPostBySlug(params.slug, [
+        'title',
+        'date',
+        'slug',
+        'author',
+        'content',
+        'ogImage',
+        'coverImage'
+    ])
+    const content = await markdownToHtml(post.content || '')
 
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
-    },
-  }
+    return {
+        props: {
+            post: {
+                ...post,
+                content
+            }
+        }
+    }
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(['slug'])
+    const posts = getAllPosts(['slug'])
 
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      }
-    }),
-    fallback: false,
-  }
+    return {
+        paths: posts.map((post) => {
+            return {
+                params: {
+                    slug: post.slug
+                }
+            }
+        }),
+        fallback: false
+    }
 }
