1568c672e27f0c8904bd6e269caa21f82e24c1a2
Lauren Clark <lauren.c4231@gmail.com>
2020-07-29 14:19:01 +0100

    Format

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,44 +8,44 @@ import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>Next.js Blog Example with {CMS_NAME}</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </Layout>
+        </>
+    )
 }
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+    const allPosts = getAllPosts([
+        'title',
+        'date',
+        'slug',
+        'author',
+        'coverImage',
+        'excerpt'
+    ])
 
-  return {
-    props: { allPosts },
-  }
+    return {
+        props: { allPosts }
+    }
 }
