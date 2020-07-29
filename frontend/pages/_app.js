6b93ad3e03f12554e0e0bb6cfb7651baf602b368
Lauren Clark <lauren.c4231@gmail.com>
2020-07-29 13:09:41 +0100

    Add tina provider and boiler

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,41 @@
 import '../styles/index.css'
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+import {
+    StrapiMediaStore,
+    StrapiProvider,
+    StrapiClient
+} from 'react-tinacms-strapi'
+import { TinaCMS, TinaProvider } from 'tinacms'
+
+import { useMemo } from 'react'
+
+export default function App({ Component, pageProps }) {
+    const cms = useMemo(
+        () =>
+            new TinaCMS({
+                toolbar: true,
+                enabled: true,
+                apis: {
+                    strapi: new StrapiClient(process.env.STRAPI_URL)
+                },
+                media: {
+                    store: new StrapiMediaStore(process.env.STRAPI_URL)
+                }
+            }),
+        []
+    )
+    return (
+        <TinaProvider cms={cms}>
+            <StrapiProvider
+                onLogin={() => {
+                    /* TODO */
+                }}
+                onLogout={() => {
+                    /* TODO*/
+                }}
+            >
+                <Component {...pageProps} />
+            </StrapiProvider>
+        </TinaProvider>
+    )
 }
