888be5cf623b105c2a8c623b3a9909f48b5bf01b
Lauren Clark <lauren.c4231@gmail.com>
2020-07-29 14:35:37 +0100

    Remove this crap

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -3,14 +3,13 @@ import Footer from '../components/footer'
 import Meta from '../components/meta'
 
 export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Meta />
+            <div className="min-h-screen">
+                <main>{children}</main>
+            </div>
+            <Footer />
+        </>
+    )
 }
