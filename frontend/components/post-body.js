78972f11424bc54a02d830e8d9c30f54e46b45e8
Lauren Clark <lauren.c4231@gmail.com>
2020-07-30 22:19:36 +0100

    Why the funk won't this style

--- frontend/components/post-body.js
+++ frontend/components/post-body.js
@@ -1,11 +1,15 @@
 import { InlineWysiwyg } from 'react-tinacms-editor'
 import ReactMarkdown from 'react-markdown'
+import markdownStyles from './markdown-styles.module.css'
 
 export default function PostBody({ content }) {
     return (
         <div className="max-w-2xl mx-auto">
             <InlineWysiwyg name="content" format="markdown">
-                <ReactMarkdown source={content} />
+                <ReactMarkdown
+                    className={markdownStyles['markdown']}
+                    source={content}
+                />
             </InlineWysiwyg>
         </div>
     )
