6f8b0f6dc4e77e9c218e9369343dd6c8936b16a1
Lauren Clark <lauren.c4231@gmail.com>
2020-07-30 21:39:43 +0100

    Swap out cover image component for inlineimage component

--- frontend/components/post-header.js
+++ frontend/components/post-header.js
@@ -1,12 +1,19 @@
+import { InlineText, InlineImage } from 'react-tinacms-inline'
+import { useCMS } from 'tinacms'
+
 import Avatar from '../components/avatar'
 import DateFormater from '../components/date-formater'
 import CoverImage from '../components/cover-image'
 import PostTitle from '../components/post-title'
 
 export default function PostHeader({ title, coverImage, date, author }) {
+    const cms = useCMS()
     return (
         <>
-            <PostTitle>{title}</PostTitle>
+            <PostTitle>
+                {/* Replace the child property with a child component with the name=title */}
+                <InlineText name="title" />
+            </PostTitle>
             <div className="hidden md:block md:mb-12">
                 <Avatar
                     name={author.name}
@@ -14,7 +21,29 @@ export default function PostHeader({ title, coverImage, date, author }) {
                 />
             </div>
             <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
-                <CoverImage title={title} src={coverImage} />
+                {/*  💬 Here we used the useCMS hook to get access to our CMS object. In
+                _app.js we've attached a Strapi media store to this object, and we need 
+                access to that here to help us resolve image urls. */}
+                <InlineImage
+                    name="coverImage.url"
+                    previewSrc={(formValues) => {
+                        process.env.STRAPI_URL +
+                            cms.media.store.getFilePath(
+                                formValues.coverImage.url
+                            )
+                    }}
+                    uploadDir={() => '/uploads'}
+                    parse={(filename) => {
+                        return `/uploads/${filename}`
+                    }}
+                >
+                    {() => (
+                        <img
+                            src={coverImage}
+                            alt={`Cover Image for ${title}`}
+                        />
+                    )}
+                </InlineImage>
             </div>
             <div className="max-w-2xl mx-auto">
                 <div className="block md:hidden mb-6">
