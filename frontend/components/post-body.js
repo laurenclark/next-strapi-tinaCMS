c587a24b519c812438718f05a9ef81b613cab87b
Lauren Clark <lauren.c4231@gmail.com>
2020-07-30 21:44:14 +0100

    Add InlineWysiwyg to the post body content and return the formatting.

--- frontend/components/post-body.js
+++ frontend/components/post-body.js
@@ -1,12 +1,12 @@
-import markdownStyles from './markdown-styles.module.css'
+import { InlineWysiwyg } from 'react-tinacms-editor'
+import ReactMarkdown from 'react-markdown'
 
 export default function PostBody({ content }) {
-  return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  )
+    return (
+        <div className="max-w-2xl mx-auto">
+            <InlineWysiwyg name="content" format="markdown">
+                <ReactMarkdown source={content} />
+            </InlineWysiwyg>
+        </div>
+    )
 }
