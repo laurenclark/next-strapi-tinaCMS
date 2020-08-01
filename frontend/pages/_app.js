79198130f90dcf2ae525e394eaad52a5a0833c09
Lauren Clark <lauren.c4231@gmail.com>
2020-08-01 12:52:31 +0100

    Enable sidebar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -19,6 +19,9 @@ export default function App({ Component, pageProps }) {
                 // Only enable for those with permissions
                 toolbar: pageProps.preview,
                 enabled: pageProps.preview,
+                sidebar: {
+                    position: 'overlay'
+                },
                 apis: {
                     strapi: new StrapiClient(process.env.STRAPI_URL)
                 },
