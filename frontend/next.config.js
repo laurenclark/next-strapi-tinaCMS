bdb31ec94f2d491e6b864e4ecdc43566759c67ba
Lauren Clark <lauren.c4231@gmail.com>
2020-08-01 21:39:33 +0100

    Add example of redirects and rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,3 +5,25 @@ module.exports = {
         STRAPI_URL: process.env.STRAPI_URL
     }
 }
+// module.exports = {
+//     async rewrites() {
+//         return [
+//             {
+//                 source: '/backend/:path*',
+//                 destination: 'https://example.com/:path*'
+//             }
+//         ]
+//     }
+// }
+
+// module.exports = {
+//     async redirects() {
+//         return [
+//             {
+//                 source: '/about',
+//                 destination: '/',
+//                 permanent: true
+//             }
+//         ]
+//     }
+// }
