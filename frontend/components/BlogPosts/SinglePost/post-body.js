ff8bb5a87a980876c54962db9e0dd203a93542ef
Lauren Clark <lauren.c4231@gmail.com>
2020-08-03 14:14:29 +0100

    Better way of styling - don't like the polluting global

--- frontend/components/BlogPosts/SinglePost/post-body.js
+++ frontend/components/BlogPosts/SinglePost/post-body.js
@@ -5,7 +5,11 @@ import markdownStyles from '../../markdown-styles.module.css'
 export default function PostBody({ content }) {
     return (
         <div className="max-w-2xl mx-auto">
-            <InlineWysiwyg name="content" format="markdown">
+            <InlineWysiwyg
+                name="content"
+                format="markdown"
+                className={markdownStyles['wysiwyg-wrapper']}
+            >
                 <ReactMarkdown
                     className={markdownStyles['markdown']}
                     source={content}
