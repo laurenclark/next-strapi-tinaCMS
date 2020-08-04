b09dc68844ef5590daaf050f1d7bc60362e71039
Lauren Clark <lauren.c4231@gmail.com>
2020-08-04 14:27:48 +0100

    Emph date

--- frontend/components/date-formater.js
+++ frontend/components/date-formater.js
@@ -2,5 +2,9 @@ import { parseISO, format } from 'date-fns'
 
 export default function DateFormater({ dateString }) {
     const date = parseISO(dateString)
-    return <time dateTime={dateString}>{format(date, 'dd LLLL yyyy')}</time>
+    return (
+        <time dateTime={dateString}>
+            <em>{format(date, 'dd LLLL yyyy')}</em>
+        </time>
+    )
 }
