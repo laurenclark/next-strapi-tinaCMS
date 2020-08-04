d48816a646506caefaa08f63033a2058bdbd4d16
Lauren Clark <lauren.c4231@gmail.com>
2020-08-04 17:30:21 +0100

    Rename to Dashboard

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     admin: {
+        url: '/dashboard',
         auth: {
             secret: env('ADMIN_JWT_SECRET', '1bd1e325d66f4c8466471892f1121be8')
         }
