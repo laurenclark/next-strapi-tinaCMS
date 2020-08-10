b83271e23d5890e52eb2ed8905439b7a591c5bfe
Lauren Clark <lauren.c4231@gmail.com>
2020-08-10 16:57:37 +0100

    🚧  WIP - cover image messes up when it should be null and escaped

--- frontend/components/blog-posts/single-post/post-header.js
+++ frontend/components/blog-posts/single-post/post-header.js
@@ -24,13 +24,13 @@ export default function PostHeader({ title, coverImage, date, author }) {
                 {/*  💬 Here we used the useCMS hook to get access to our CMS object. In
                 _app.js we've attached a Strapi media store to this object, and we need 
                 access to that here to help us resolve image urls. */}
-                {coverImage.url && (
+                {coverImage && (
                     <InlineImage
                         name="coverImage.url"
                         previewSrc={(formValues) => {
                             process.env.STRAPI_URL +
                                 cms.media.store.getFilePath(
-                                    formValues.coverImage.url
+                                    formValues?.coverImage?.url
                                 )
                         }}
                         uploadDir={() => '/uploads'}
