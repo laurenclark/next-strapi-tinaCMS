236d3adaec1823237c5dc130385560428caf10f7
Lauren Clark <lauren.c4231@gmail.com>
2020-08-11 17:08:39 +0100

    Remove

--- frontend/components/Globals/header.js
+++ frontend/components/Globals/header.js
@@ -37,14 +37,6 @@ export default function Header() {
                         </a>
                     </Link>
                 </div>
-                <div>
-                    <a
-                        href="#"
-                        className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0 text-2xl"
-                    >
-                        Edit
-                    </a>
-                </div>
             </div>
         </nav>
     )
